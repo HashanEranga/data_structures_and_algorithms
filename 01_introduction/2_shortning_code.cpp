48d8fbcc6f350411e24d7d68abd15f7172e2312a
= <ccmrhe@gmail.com>
2021-02-16 02:31:51 -0500

    implementing shorting code

---
+++ 01_introduction/2_shortning_code.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+int main(){
+    
+    ll a = 123456789;
+    ll b = 987654321;
+
+    cout << a*b << endl;
+
+    return 0;
+}
+/* 
+
+Note :
+    short coding is ideal in competitive programming. because the programme 
+    written as fast as possible, 
+    name long long is long it can be shorter as follows.
+
+    typedef long long ll;
+
+    ex:
+        long long a = 123454567;
+        long long b = 123456789;
+        cout << a+b << endl;
+
+        can be shorten as following 
+
+        ll a = 1234556789;
+        ll b = 9876543211;
+
+        cout << a+b << endl;
+
+        typedef also use as follows for complex stuff
+        typedef vector<int> vi;
+        typedef pait<int, int> pi;
+
+
+ */
