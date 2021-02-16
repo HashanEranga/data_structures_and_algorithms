c56d07c5f5f3f128adeeddf23de68b9c4d758286
= <ccmrhe@gmail.com>
2021-02-16 03:13:07 -0500

    initialize macros

---
+++ 01_introduction/2_macros.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    
+
+    return 0;
+}
+
+/* 
+
+Note : 
+    this is another way of shorting the code 
+    macro makes certain strings in the code will be 
+    changed before the compilationusing #define keyword
+    ex 
+
+ */
