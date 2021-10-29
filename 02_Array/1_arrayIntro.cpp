307d23577aff44be834b23d780aa6de76b5dd495
Hashan Eranga <ccmrhe@gmail.com>
2021-10-29 16:20:29 +0530

    done(): array intro

---
+++ 02_Array/1_arrayIntro.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // creating an array 
+    // data_type array_name[size]
+
+    // creating an array with 100 elements.
+    // all the elements will be garbage values
+    // allocate static memory of 400 bytes (4bytes x 100) 
+    // int a[100];
+
+    // creating an array initializing first element as 0
+    // eventually all the other elements will be 0 
+    // int a[100] = {0};
+
+    // define first 3 elements and all the other elements will be zero
+    // int a[100] = {1,2,3};
+
+    // creating an array with size 3 elements. 
+    // int a[] = {1,2,3};
+
+    // creating an string array
+    // in character arrays we need to create null character and terminate a character array
+    string fruits[] = {"apple", "mango", "guava"};
+
+    cout << "Create(): array intro cpp file" << endl;
+    return 0;
+}
