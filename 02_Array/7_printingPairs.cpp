0ce346cfce4da2407c70041553a1117713116c3f
Hashan Eranga <ccmrhe@gmail.com>
2021-10-30 15:22:51 +0530

    create(): printing pairs

---
+++ 02_Array/7_printingPairs.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void printingPairs(int * arr, int size);
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int sizeofArr = sizeof(arr)/sizeof(arr[0]);
+
+    printingPairs(arr, sizeofArr);
+    return 0;
+}
+
+void printingPairs(int * arr, int size){
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            cout << "(" << arr[i] << ", " << arr[j] << ") "; 
+        }
+        cout << endl;
+    }
+}
