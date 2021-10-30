12b5eb3f10475c3e4a8c01b078c09682c5edc0af
Hashan Eranga <ccmrhe@gmail.com>
2021-10-30 15:06:41 +0530

    create() : reversing array method

---
+++ 02_Array/6_reverseArray.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+// method for display array items
+void dispArr(int * arr, int size);
+
+// method fot reverse array items
+void reverseArr(int * arr, int size);
+
+int main(int argc, char const *argv[])
+{
+    // sample array
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+
+    // size of array 
+    int sizeofArr = sizeof(arr)/sizeof(arr[0]);
+
+    // display array elements
+    dispArr(arr, sizeofArr);
+
+    // reverse array elements
+    reverseArr(arr, sizeofArr);
+
+    // display array elements
+    dispArr(arr, sizeofArr);
+    return 0;
+}
+
+void dispArr(int * arr, int size){
+    for(int i=0;i<size;i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void reverseArr(int * arr, int size){
+    int start, end; 
+    start = 0;
+    end = size-1;
+
+    while(start<end){
+        swap(arr[start], arr[end]);
+        start+=1;
+        end-=1;
+    }
+
+}
