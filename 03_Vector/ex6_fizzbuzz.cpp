fd4a44ddb4556d8c216be41427237656afc9ffc5
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-11-11 18:17:37 +0530

    create() : initiate fizzbuzz code

---
+++ 03_Vector/ex6_fizzbuzz.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+vector<string> fizzbuzz(int n){
+    for(int i=0;i<n;i++){
+        
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    fizzbuzz(15);k
+    return 0;
+}
