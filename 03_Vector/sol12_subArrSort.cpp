eec7aa233aaf30ce57eac817fe61c2656383e655
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-11-19 00:10:10 +0530

    solved(): subarr sort using optimization

--- 03_Vector/sol12_subArrSort.cpp
+++ 03_Vector/sol12_subArrSort.cpp
@@ -18,12 +18,41 @@ vector<int> subArrSortBruteForce(vector<int> arrUnSort){
     return {i,j};
 }
 
-vector<int> subArraySort
+bool outOfOrder(vector<int> arr, int i){
+    if(i==0) return arr[i]>arr[1];
+    if(i==arr.size()-1) return arr[i]<arr[arr.size()-2];
+    
+    return arr[i] < arr[i-1] or arr[i] > arr[i+1];
+}
+
+vector<int> subArraySort(vector<int> arr){
+    int smallest=INT_MAX;
+    int largest=INT_MIN;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(outOfOrder(arr,i)){
+            smallest=min(smallest, arr[i]);
+            largest=max(largest, arr[i]);
+        }
+    }
+
+    if(smallest==INT_MAX) return{-1,-1};
+
+    int left=0; int right=arr.size()-1;
+
+    while(smallest>=arr[left]) left++;
+    while(largest<=arr[right]) right--;
+
+    return {left, right};
+}
+
 
 int main(int argc, char const *argv[])
 {
     vector<int> arr{1,2,3,4,5,8,7,6,9,10,11};
-    vector<int> ans=subArrSortBruteForce(arr);
+    // vector<int> ans=subArrSortBruteForce(arr);
+    vector<int> ans=subArraySort(arr);
 
     cout << ans[0] << endl;
     cout << ans[1] << endl;
