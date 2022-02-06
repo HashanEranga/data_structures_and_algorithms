0a89a22d1cece69944fb00450bdfc9951e24e1e1
Hashan Eranga <ccmrhe@gmail.com>
2022-02-06 14:09:01 +0530

    hackerrank(): Java | static initializer block sol

--- cpp/devEnv/subArrSumPrefSum.cpp
+++ cpp/devEnv/subArrSumPrefSum.cpp
@@ -6,18 +6,25 @@ using namespace std;
 int subArrSumPrefSum(vector<int> arr){
 	
 	int n=arr.size();
-	int largestSum=INT_MIN;
+	int largestSum=0;
+	
 	// prefix array
 	vector<int> prefArr;
 	prefArr[0]=arr[0];
-	for(int i=1;i<n;i++) prefArr[i] = prefArr[i-1]+arr[i];
+
+	for(int i=1;i<n;i++){
+		prefArr[i] = prefArr[i-1]+arr[i];
+	}
 
 	for(int i=0;i<n;i++){
 		for(int j=1;j<n;j++){
-			int currentSum =(i>0) ? prefArr[j]-prefArr[i-1] : prefArr[j];
-			largestSum=max(largestSum,currentSum);
+			int currentSum;
+			if(i!=0) currentSum=prefArr[j]-prefArr[i-1];
+			else currentSum=prefArr[j];
+			if(largestSum<currentSum) largestSum=currentSum;
 		}
 	}
+
 	return largestSum;
 }
 
