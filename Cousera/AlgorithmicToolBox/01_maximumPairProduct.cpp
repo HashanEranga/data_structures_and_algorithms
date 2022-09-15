73680624fcdea1e3d2d39b3b4dc9864cbb9b967a
HashanEranga <ccmrhe@gmail.com>
2022-09-15 20:07:52 +0530

    get the maximum number and the next number and mul

--- Cousera/AlgorithmicToolBox/01_maximumPairProduct.cpp
+++ Cousera/AlgorithmicToolBox/01_maximumPairProduct.cpp
@@ -4,11 +4,23 @@
 using namespace std;
 
 int getMaxPairwiseProduct(vector<long long> arr){
-    long long maxValue = 0;
+    // long long maxValue = 0;
+    // for (int i = 0; i < arr.size(); i++)
+    //     for (int j = i+1; j < arr.size(); j++)
+    //         if(arr[i]*arr[j] > maxValue) maxValue = arr[i]*arr[j];
+    // return maxValue;
+
+    long long firstMaxNum = 0;
+    long long secondMaxNum = 0;
+
     for (int i = 0; i < arr.size(); i++)
-        for (int j = i+1; j < arr.size(); j++)
-            if(arr[i]*arr[j] > maxValue) maxValue = arr[i]*arr[j];
-    return maxValue;
+        if(arr[i]> firstMaxNum) firstMaxNum = arr[i];
+
+    for (int i = 0; i < arr.size(); i++)
+        if(arr[i]> secondMaxNum & arr[i] != firstMaxNum) secondMaxNum = arr[i];
+    
+    return firstMaxNum * secondMaxNum;
+
 }
 
 int main(){
